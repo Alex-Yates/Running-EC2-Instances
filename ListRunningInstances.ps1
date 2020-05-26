eab98ecfd0b719688ae1a65390b25bb5f9e7977d
Alex Yates <alex.yates@dlmconsultants.com>
2020-05-26 16:02:07 +0100

    It works on my machine

--- ListRunningInstances.ps1
+++ ListRunningInstances.ps1
@@ -1,48 +1,52 @@
-# To do - Write this PS
-Write-Output "Starting script"
+# These are the states that an EC2 VM can be in
+# (I'm doing it this way to ensure I've considered all possible states. If it ever becomes possible for an instance
+# to be in a different state, and one of my instance enters that state, this script should throw and error)
+[array]$expectedStates = "pending","running","shutting-down","terminated","stopping","stopped"
 
+# Asking EC2 what VMs I have running
 $states = @()
-
 Get-EC2Instance | ForEach-Object {	 
-	 $_.RunningInstance | ForEach-Object {
-	 	Write-Output $("Instance {0} is in the state {1}" -f $_.InstanceId, $_.State.Name)
-		Write-Output "Checking State.Name actually holds a value"
+	$_.RunningInstance | ForEach-Object {
+		$instanceId = (Get-EC2Instance $_.InstanceId).Instances.InstanceId
 		$instancestate = (Get-EC2Instance $_.InstanceId).Instances.State.Name
-		Write-Output "instantstate is: $instancestate"
-		Write-Output "Adding $instancestate to states" 
+		$msg = "$instanceId is in the state: $instancestate"
+		Write-Output $msg
 		$states += $instancestate
-	 }
+	}
 }
 
-Write-Output "Logging all the states"
-$states | ForEach-Object {
-	Write-Output "There is an instance in the state: " + $_
+# Checking to see if I have any VMs that are costing me money
+$VMsRunning = $false
+if (($states -contains "running") -or ($states -contains "pending")){
+	$VMsRunning = $true
 }
 
-$expectedStates = "pending","running","shuttingDown","terminated","stopping","stopped"
-$pending = $states.Length | Where-Object {$_ -like "pending"}
-$running = $states.Length | Where-Object {$_ -like "running"}
-$shuttingDown = $states.Length | Where-Object {$_ -like "shutting-down"}
-$terminated = $states.Length | Where-Object {$_ -like "terminated"}
-$stopping = $states.Length | Where-Object {$_ -like "stopping"}
-$stopped = $states.Length | Where-Object {$_ -like "stopped"}
-$unexpectedStates = $states.Length | Where-Object {$_ -notin $expectedStates}
+# Creating a summary of the status of all my VMs
+$returnMsg = "Summary of existing EC2 instances: "
+forEach ($state in $expectedStates){	
+	$count = $states | Where-Object {$_ -like $state}
+	$suffix = $state + ": " + $count.Length + ", "
+	$returnMsg += $suffix
+}
 
-$returnMsg = "Summary of current instances - Pending: $pending, Running: $running, Shutting down: $shuttingDown, Terminated: $terminated, Stopping: $stopping, Stopped: $stopped, Unexpected states: $unexpectedStates"
+# Checking if there are any VMs in any other state
+[array]$unexpectedStates = $states | Where-Object {$_ -notin $expectedStates}
+$SumUnexpectedStates = $unexpectedStates.Length
+$returnMsg = $returnMsg + "Unexpected states: $SumUnexpectedStates"
 
-if ($unexpectedStates -gt 0){
+# Writing errors
+if ($SumUnexpectedStates -gt 0){
 	Write-Warning "D'oh! Looks like the build script is failing to recognise some instance states! Better check your code."
 }
-if (($pending -gt 0) -or ($running -gt 0) ){
+if ($VMsRunning){
 	Write-Warning "D'oh! Looks like you've left some EC2 instances running overnight! If you aren't using them, best turn them off to save the pennies."
 }
 
-if (($pending -gt 0) -or ($running -gt 0) -or ($unexpectedStates -gt 0)){
+# Returning the good/bad news
+if ($VMsRunning -or ($SumUnexpectedStates -gt 0)){
 	Write-Error $returnMsg
-}
+} 
 else {
-	Write-Output "Good news! You don't have any expensive EC2 instances butning a hole in your wallet!"
+	Write-Output "Good news! You don't have any expensive EC2 instances burning a hole in your wallet!"
 	Write-Output $returnMsg
 }
-
-Write-Output "Finishing script"
